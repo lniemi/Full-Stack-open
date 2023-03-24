c5cc39a6706950b07ee2709de84d1d7d6acf9711
lniemi <90403204+lniemi@users.noreply.github.com>
2023-03-24 19:27:03 +0200

    Step 3 (Part 2.8)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
   event.preventDefault()
   const personObject = {
-    name: newName
+    name: newName,
+    number: newNumber
   }
   
   const isDuplicate = persons.some(person => person.name === newName)
@@ -19,12 +21,17 @@ const App = () => {
   } else {
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 }
 
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,12 +42,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person => 
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>{person.name} {person.number}</p>
         )}
     </div>
   )
