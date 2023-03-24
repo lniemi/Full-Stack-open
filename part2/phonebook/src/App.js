c96d28072317da42605392ab63338790915f3322
lniemi <90403204+lniemi@users.noreply.github.com>
2023-03-24 21:13:17 +0200

    Step 5 (Part 2.10)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,75 +1,100 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const Filter = ({ filter, handleFilterChange }) => (
+  <div>
+    filter shown with <input value={filter} onChange={handleFilterChange} />
+  </div>
+);
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const Person = ({ person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+);
+
+const Persons = ({ filteredPersons }) => (
+  <div>
+    {filteredPersons.map((person) => (
+      <Person key={person.name} person={person} />
+    ))}
+  </div>
+);
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = (event) => {
-  event.preventDefault()
-  const personObject = {
-    name: newName,
-    number: newNumber
-  }
-  
-  const isDuplicate = persons.some(person => person.name === newName)
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
 
-  if (isDuplicate) {
-    alert(`${newName} is already added to phonebook`)
-  } else {
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
-}
+    const isDuplicate = persons.some((person) => person.name === newName);
 
-  
+    if (isDuplicate) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
 
-  const filteredPersons = persons.filter(person =>
+  const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filter.toLowerCase())
-  )
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
       <h2>Numbers</h2>
-      {filteredPersons.map(person =>
-          <p key={person.name}>{person.name} {person.number}</p>
-        )}
+      <Persons filteredPersons={filteredPersons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
