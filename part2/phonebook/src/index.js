4adc7a5922dc8c876a212af42f6f69cfc7c79c0b
lniemi <90403204+lniemi@users.noreply.github.com>
2023-04-09 02:44:43 +0300

    Part 3 exercise, Step 9 (3.9)
    Part 3 repo: https://github.com/lniemi/Full-Stack-open-Part3

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -4,7 +4,6 @@ import "./index.css"
 
 import App from './App'
 
-axios.get('http://localhost:3001/persons').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+axios.get('http://localhost:3001/api/persons').then(response => {
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)})
