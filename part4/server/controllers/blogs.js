50b39b69a06846201c1a1ed960fda0bd5c3183e0
lniemi <90403204+lniemi@users.noreply.github.com>
2023-06-12 19:55:11 +0300

    Step 1 (4.3)

--- part4/server/controllers/blogs.js
+++ part4/server/controllers/blogs.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const Blog = require('../models/blog');
+const express = require('express')
+const Blog = require('../models/blog')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/api/blogs', (req, res) => {
   Blog
     .find({})
     .then((blogs) => {
-      res.json(blogs);
+      res.json(blogs)
     })
-});
+})
 
 router.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body);
+  const blog = new Blog(req.body)
 
   blog
     .save()
     .then((result) => {
-      res.status(201).json(result);
+      res.status(201).json(result)
     })
-});
+})
 
-module.exports = router;
+module.exports = router
--- part4/server/index.js
+++ part4/server/index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const routes = require('./controllers/blogs');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const routes = require('./controllers/blogs')
 
-const app = express();
+const app = express()
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use(routes);
+app.use(routes)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+  console.log(`Server running on port ${process.env.PORT}`)
+})
