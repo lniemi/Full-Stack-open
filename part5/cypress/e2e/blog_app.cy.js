2c0b3ac685b7d47bc6176bf536dfbc398b0604f9
lniemi <90403204+lniemi@users.noreply.github.com>
2023-09-18 21:04:38 +0300

    Step 6 (Part 5.22)

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -15,7 +15,13 @@ describe('Blog', function () {
       username: 'Ttestaaja',
       password: 'salainen',
     }
+    const user2 = {
+      name: 'Second User',
+      username: 'seconduser',
+      password: 'password2'
+    }
     cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user2)
     cy.visit('')
   })
 
@@ -78,6 +84,23 @@ describe('Blog', function () {
       cy.wait(1000) // 1s
       cy.on('window:confirm', () => true)
       cy.contains('Blog to be deleted').should('not.exist')
+    })
+    it('Only creator can delete a blog', function () {
+      cy.contains('Add New Blog').click()
+      cy.get('#title').type('Tepon eka blogi')
+      cy.get('#author').type('Teppo Testaaja')
+      cy.get('#url').type('www.testiblogi.fi')
+      cy.get('#create-button').click()
+      cy.contains('Tepon eka blogi')
+      cy.contains('logout').click()
+      cy.wait(1000) // 1s
+
+      cy.get('#username').type('seconduser')
+      cy.get('#password').type('password2')
+      cy.get('#login-button').click()
+      cy.contains('Tepon eka blogi').parent().as('blogRow')
+      cy.get('@blogRow').contains('view').click()
+      cy.get('@blogRow').should('not.contain', 'Delete')
     })
   })
 })
