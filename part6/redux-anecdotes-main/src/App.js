a5c072636e4b38f93444e38bc8dd5c668bcd9621
lniemi <90403204+lniemi@users.noreply.github.com>
2023-09-22 03:31:26 +0300

    Step 4 (Part 6.6)

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,18 +10,12 @@ const App = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch({
-      type: 'CREATE',
-      data: { content }
-    })  
+    dispatch(createAnecdote(content))  
   }
 
   const vote = (id) => {
     console.log('vote', id)
-    dispatch({
-      type: 'VOTE',
-      data: { id }
-    })
+    dispatch(voteAnecdote(id))
   }
 
   return (
--- part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -19,6 +19,24 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'CREATE',
+    data: {
+      content,
+      id: (100000 * Math.random()).toFixed(0),
+      votes: 0
+    }
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
